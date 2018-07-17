4e22a07d059f417c2d5b620f11bf39fe2b4bb349
Vanessa Keller <33420882+Nessy1512@users.noreply.github.com>
2018-07-17 09:48:16 +0200

    Update StartJenaFuseki.java

--- Java/Demo without other components/src/startJenaFuseki/StartJenaFuseki.java
+++ Java/Demo without other components/src/startJenaFuseki/StartJenaFuseki.java
@@ -1,24 +1,36 @@
 package startJenaFuseki;
 
+import java.io.InputStream;
+import java.nio.file.Paths;
+
+import org.apache.jena.fuseki.embedded.FusekiServer;
 import org.apache.jena.query.Dataset;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.sparql.core.DatasetImpl;
-import org.apache.jena.fuseki.embedded.FusekiServer;
+import org.apache.jena.util.FileManager;
 
+/**
+ * @author Robert Rapp, Jennifer Tran, Vanessa Keller.
+ * 
+ */
 
 public class StartJenaFuseki {
 
+	/**
+	 * Speichert die Ontologie auf den lokalen Server und startet diesen.
+	 * 
+	 */
+	
 	public static void main(String[] args) {
-		
-		// source: https://jena.apache.org/documentation/javadoc/fuseki2-embedded/org/apache/jena/fuseki/embedded/FusekiServer.html
-		
-				Model model = ModelFactory.createDefaultModel() ;
-				model.read("\\Ontology\Wissensbasis- Dokumente.owl");
-				Dataset ds = new DatasetImpl(model);
-				FusekiServer server = FusekiServer.make(3030, "ds", ds.asDatasetGraph());
-				server.start();
-
-	}
+		Model model = ModelFactory.createDefaultModel() ;	
+		String rootStorePath = Paths.get("ontology.owl").toString();
+		System.out.println(rootStorePath);
+		model.read(rootStorePath);
+		System.out.println("modelreaded"+model.toString());
+		Dataset ds = new DatasetImpl(model);
+		FusekiServer server = FusekiServer.make(3030, "ds", ds.asDatasetGraph());
+		server.start();
+}
 
 }
